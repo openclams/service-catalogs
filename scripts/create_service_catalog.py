945b2248abd1c2b87516c5d7fcf1550c1f41db27
Otto Bibartiu <otto.bibartiu@ipvs.uni-stuttgart.de>
2019-11-28 23:40:52 +0100

    Update create service prog

--- scripts/create_service_catalog.py
+++ scripts/create_service_catalog.py
@@ -3,20 +3,17 @@ import os
 import json
 from typing import List, Set, Dict, Tuple, Optional, Any
 import jsonschema
+import argparse
+import re
 
-output_file:str = "../test/service-catalog.json"
-path:str = "../test/partials"
-schema_path:str = "../schemas/service-catalog.json"
-
-
-def read_JSON_from_file() ->  Dict[str,Any]:
+def read_JSON_from_file(path:str,schema_path:str) ->  Dict[str,Any]:
     json_fragments_list: Dict[str,Any] = {}
     with open(schema_path, 'r') as schema_file:
         json_schema = json.load(schema_file)
 
 
     for json_file_path in glob.glob(path+"/*.json"):
-        print("-> Open JSON Fragment: ",json_file_path)
+        print("-> Found JSON Files: ",json_file_path)
         with open(json_file_path) as json_file:
             json_data = json.load(json_file)
             try:
@@ -38,22 +35,19 @@ def read_JSON_from_file() ->  Dict[str,Any]:
                     print("Node with Id:'"+node_id+"'is ignored.")
     return json_fragments_list
 
-# def make_tree_structure(json_fragments_list: Dict[str,Any]) -> Any:
-#     tree:List[Any] = []
-#     node_ids = list(json_fragments_list.keys())
-#     for node_id in node_ids:
-#         if node_id in json_fragments_list:
-#             tree_node = json_fragments_list[node_id]
-#             node_objs = get_child_with_children(tree_node,json_fragments_list,[])
-#             tree.append(node_objs)
-#     return tree    
 
 def make_tree_structure(json_fragments_list: Dict[str,Any]) -> Any:
     tree:List[Any] = []
     root_ids = find_root_nodes(json_fragments_list)
-    is_used_list:List[str] = []
+    is_used_list_global:List[str] = []
     for root_node_id in root_ids:
-        tree.append(get_child_with_children(root_node_id,json_fragments_list,is_used_list))    
+        is_used_list:List[str] = []
+        tree.append(get_child_with_children(
+                                root_node_id,
+                                json_fragments_list,
+                                is_used_list,
+                                is_used_list_global)
+                    )    
 
     return tree
 
@@ -66,30 +60,32 @@ def find_root_nodes(json_fragments_list):
     return [node_id for node_id in json_fragments_list if node_id not in list_of_all_children_ids]
 
 
-def get_child_with_children(node_id:Any, json_fragments_list:Dict[str,Any],is_used_list:List[str]):
+def get_child_with_children(node_id:Any, json_fragments_list:Dict[str,Any],is_used_list:List[str],is_used_list_global:List[str]):
     if node_id in is_used_list:
-        pass
-        # print("Error: Circualar structure detected around Id:",child_node["id"])
-        # print("Abort!")
-        # exit(0)
+        print("Error: Circualar structure detected around Id:",node_id)
+        print("Abort!")
+        exit(0)
     else:
         is_used_list.append(node_id)
-
+    
+    is_used_list_global.append(node_id)
     node = json_fragments_list[node_id]
 
     if node["type"] == "Node" and  "children" in node and len(node["children"]):
+        #At the begining, the children propertie is a list of ids of type string.
+        #We replace piecewise each string with the actual object recursivley.
         for idx in range(len(node["children"])):
             node_obj = node["children"][idx]
-            #Check if this node has all ids replaced yet
+           
             if not isinstance(node_obj, str):
                 continue
-            
-            #We did not iterate over this node
-            #Replace all Ids with their corresponding children nodes
+
             if node_obj in json_fragments_list:
                 child_node = json_fragments_list[node_obj]
+                #if child_node['id'] in is_used_list_global:
+                #TODO  child_node = branch_node_create_copy(child_node,json_fragments_list,is_used_list_global)
                 node["children"][idx] = child_node
-                get_child_with_children(child_node["id"], json_fragments_list, is_used_list)
+                get_child_with_children(child_node["id"], json_fragments_list, is_used_list,is_used_list_global)
             else:
                 child_node["children"].remove(node_obj) 
                 print("Error: Id of child node no found.")
@@ -98,71 +94,16 @@ def get_child_with_children(node_id:Any, json_fragments_list:Dict[str,Any],is_us
 
     return  node  
 
-# def get_child_with_children(child_node:Any, json_fragments_list:Dict[str,Any],is_used_list:List[str]):
-#     if child_node["id"] in is_used_list:
-#         print("Error: Circualar structure detected around Id:",child_node["id"])
-#         print("Abort!")
-#         exit(0)
-#     else:
-#         is_used_list.append(child_node["id"])
+def branch_node_create_copy(node,json_fragments_list,is_used_list_global):
+        new_node = node.copy()
+        if re.match(r'.*\_\d+$',node['id']):
+            new_node['id'] = re.sub(r'(\d+)$', lambda x: str(int(x.group(0)) + 1), node['id'])
+        else:
+            new_node['id'] = node['id']+"_1"
+        print("-> Node ",node['id'],"has mutltiple parents; create new subtree by inserting a copy with Id",new_node['id'])
+        json_fragments_list[new_node['id']] = new_node
+        return new_node
 
-#     if child_node["type"] == "Node" and  "children" in child_node and len(child_node["children"]):
-#         children_objs = child_node["children"]
-#         child_node_buffer = []
-#         for node_obj in children_objs:
-            
-#             #Check if this node has all ids replaced yet
-#             if not isinstance(node_obj, str):
-#                 return child_node
-            
-#             #We did not iterate over this node
-#             #Replace all Ids with their corresponding children nodes
-#             if node_obj in json_fragments_list:
-#                 node = json_fragments_list[node_obj]
-#                 child_nodes = get_child_with_children(node, json_fragments_list, is_used_list)
-#                 child_node_buffer.append(child_nodes)
-#             else:
-#                 child_node["children"].remove(node_obj) 
-#                 print("Error: Id of child node no found.")
-#                 print("Node Id:",node_obj,'in parent node',child_node["id"],"not found.")
-#                 print("Node with Id:'"+node_obj+"'is ignored.")
-#         child_node["children"] = child_node_buffer.copy()
-      
-#     del json_fragments_list[child_node["id"]]
-#     return  child_node     
-
-#This methode removes subtress that are already existing in bigger trees
-# Example: Two entries: 
-# 1. database--> key-value-storage--> redis 
-# 2. key-value-storage--> redis 
-# The 2. entry will be removed, because it already has a parent in entry 1.
-def remove_duplicate_sup_trees(tree):
-    mark_for_removal = []
-    for node_a in tree:
-        if not node_a["type"] == "Node":
-            continue
-        
-        for node_b in tree: 
-            if not node_a["type"] == "Node" or node_a["id"] == node_b["id"]:
-                continue
-
-            if(is_sub_tree(node_a,node_b)):
-                print("-> Found sub-tree for id",node_a["id"],"in",node_b["id"])
-                mark_for_removal.append(node_a) 
-
-    for node in mark_for_removal:
-        tree.remove(node)
-    return tree
-
-def is_sub_tree(node_a, node_b):
-    if node_a["id"] == node_b["id"]:
-        return True
-
-    if "children" in node_b:
-        for child in node_b["children"]:
-            return is_sub_tree(node_a,child)
-
-    return False
 
 def inherit_attributes_from_parents(tree):
     for node in tree:
@@ -197,14 +138,29 @@ def print_tree_node(node=None,depth=0):
         for child in node["children"]: 
             print_tree_node(child,depth+1)
 
-def main():    
-    json_fragments_list = read_JSON_from_file()
+def main(output_file,path,schema_path):    
+    json_fragments_list = read_JSON_from_file(path,schema_path)
     tree = make_tree_structure(json_fragments_list)
-    super_tree = tree #remove_duplicate_sup_trees(tree)
-    inherit_attributes_from_parents(super_tree)
-    print_tree(tree)
-    generate_json_output(super_tree,output_file)
+    inherit_attributes_from_parents(tree)
+    generate_json_output(tree,output_file)
+    return tree
     
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(
+        prog='Create Service Catalog',
+        description='Compile partial JSON fragments of a service catalog to full service catalog description.')
+    parser.add_argument('--verbose', '-v', action='count', default=1)
+    parser.add_argument('--version', action='version', version='%(prog) Version 0.1')
+    parser.add_argument('--schema','-s', default="../schemas/service-catalog.json", nargs='?')
+    parser.add_argument('--partials','-p', default="../test/partials", nargs='?' )
+    parser.add_argument('--output','-o', default="../test/service-catalog.json", nargs='?')
+    args = parser.parse_args()
+    output_file:str = args.output
+    path:str = args.partials
+    schema_path:str = args.schema
+    verbose = args.verbose
+    tree = main(output_file,path,schema_path)
+
+    if verbose:
+        print_tree(tree)
 
