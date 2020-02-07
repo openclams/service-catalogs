5e88731997d59cfb614ba0f3ed5b3444dab46e08
MCoskuner <st100147@stud.uni-stuttgart.de>
2020-02-07 12:00:57 +0100

    Service Catalog Upgrade

--- scripts/server.py
+++ scripts/server.py
@@ -2,13 +2,13 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 
+
 class CORSHandler(SimpleHTTPRequestHandler):
     def end_headers(self):
         self.send_header('Access-Control-Allow-Origin', '*')
         self.send_header('Access-Control-Allow-Methods', 'GET')
         self.send_header('Cache-Control', 'no-store, no-cache, must-revalidate')
         return super(CORSHandler, self).end_headers()
-
 
 httpd = HTTPServer(('localhost', 8000), CORSHandler)
 httpd.serve_forever()
